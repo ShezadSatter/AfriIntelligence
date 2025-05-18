2ea1b505fc196528b82dc9a962e9fd9bb9a8fa03
ShezadSatter <102970045+ShezadSatter@users.noreply.github.com>
2025-05-18 21:09:17 +0200

    4

--- src/pages/selection.tsx
+++ src/pages/selection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "/selectionstyle.css";
+import "./selectionstyle.css";
 
 const SelectionPage: React.FC = () => {
   return (
