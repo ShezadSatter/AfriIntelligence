3c25a08165fe469ccea21fb74a3e0822c139407b
ShezadSatter <102970045+ShezadSatter@users.noreply.github.com>
2025-05-18 21:37:06 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/global.css"; // If you have global styles
+import "./pages/index.css"; // If you have global styles
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
