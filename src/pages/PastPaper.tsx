ba1192977b5767d5d3f4eca6b06112510efe1cc5
ShezadSatter <102970045+ShezadSatter@users.noreply.github.com>
2025-08-09 23:21:29 +0200

    Import Error

--- src/pages/PastPaper.tsx
+++ src/pages/PastPaper.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PastPaper from "../types/PastPaper";
-import { fetchPapers } from "../utils/pastPaper";
+import fetchPapers from "../utils/pastPaper";
 import PastPaperList from "../components/PastPaperList";
 import PastPaperFilter from "../components/PastPaperFilter";
 
