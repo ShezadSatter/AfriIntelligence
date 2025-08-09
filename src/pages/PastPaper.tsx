e3655d9473a45c9b08d410e43ab46a7838ffded1
ShezadSatter <102970045+ShezadSatter@users.noreply.github.com>
2025-08-09 23:08:09 +0200

    File PAth

--- src/pages/PastPaper.tsx
+++ src/pages/PastPaper.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import type { PastPaper } from "../types/PastPaper";
+import PastPaper from "../types/PastPaper";
 import { fetchPapers } from "../utils/pastPaper";
 import PastPaperList from "../components/PastPaperList";
 import PastPaperFilter from "../components/PastPaperFilter";
