a1e63b8c2362e063d6a678543c0e53db162857d9
ShezadSatter <102970045+ShezadSatter@users.noreply.github.com>
2025-09-22 17:42:43 +0200

    styling up to dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,12 +6,9 @@ import '../styles/navbar.module.css';
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Welcome to Afri-Intelligence</div>
       <div>
         <ul className="nav-links">
-          <label htmlFor="checkbox_toggle" className="hamburger">
-            &#9776;
-          </label>
+          
           <div className="menu">
             <li>
               <Link to="/">Home</Link>
