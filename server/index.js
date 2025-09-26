32a360f96fa886405f12168a260d24bbed847094
jplrx <jplrx12@gmail.com>
2025-09-27 00:21:12 +0200

    Update index.js cors allowed origin

--- server/index.js
+++ server/index.js
@@ -47,6 +47,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3001",
   "https://afri-intelligence.vercel.app",
+  "https://afri-intelligence.onrender.com",
 ];
 
 app.use(cors({
